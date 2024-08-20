0b81f2630a342ab97c774c8c7e4dbb4744479b07
BenwCr <bwcrouse15@gmail.com>
2024-08-19 22:58:37 -0500

    forward and back player works

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
     pygame.init()
-    plyr = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 100)
+    plyr = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     timeClock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen,(0,0,0)) #fill black
+        plyr.draw(screen) #draw Player
+        plyr.update(dt)
         pygame.display.flip() #screen refresh/update
         dt = timeClock.tick(60)/1000
-        plyr.draw(screen)
 
 
         for event in pygame.event.get(): #quit if window is closed
--- player.py
+++ player.py
@@ -1,9 +1,9 @@
 from circleshape import CircleShape
+from constants import *
 import pygame
 class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
-        #self.position = pygame.Vector2(x, y)
         self.rotation = 0
         #self.position = pygame.Vector2(x, y)
         #self.velocity = pygame.Vector2(0, 0)
@@ -18,5 +18,23 @@ class Player(CircleShape):
         return [a, b, c]
         
     def draw(self, screen):
-        pygame.draw.polygon(screen,(255,255,255), self.triangle(),20)
-        print(self.triangle())
+        pygame.draw.polygon(screen,(255,255,255), self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TRUN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt * -1)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
