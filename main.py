b6e15eb4a9873c18a120ce50356f82f34042d8e1
BenwCr <bwcrouse15@gmail.com>
2024-08-19 21:29:19 -0500

    added FPS limit of 60

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     
     pygame.init()
+    timeClock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         pygame.Surface.fill(screen,(0,0,0)) #fill black
         pygame.display.flip() #screen refresh/update
-        for event in pygame.event.get():
+        dt = timeClock.tick(60)/1000
+        print(dt)
+
+
+        for event in pygame.event.get(): #quit if window is closed
             if event.type == pygame.QUIT:
                 return
+            
 
 
 
